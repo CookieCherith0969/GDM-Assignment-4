769edb8789c5f992751ddc0aedb4a1ba7b4a9b80
ronincampbell <ronincampbell3@gmail.com>
2024-04-05 15:26:15 +1100

    Fixed lamp plant timer

--- Scripts/lampPlant.gd
+++ Scripts/lampPlant.gd
@@ -1,22 +1,27 @@
 extends CharacterBody2D
 
 var lampActive = false
+var timer = null
 
-# Start with lamp off -R
 func _ready():
-	$AnimatedSprite2D.play("off") 
-
-# When player enters range turn lamp on -R
+	$AnimatedSprite2D.play("off") # Start with lamp off -R
+	timer = get_node("Timer") # Get timer -R
+	
+# When player enters range turn lamp on and stop any previous timer -R
 func _on_detection_area_body_entered(body):
 	if not is_instance_of(body, Player):
 		return
 	$AnimatedSprite2D.play("on")
 	lampActive = true
+	timer.stop()
 
-# When player leaves wait x seconds then turn off -R
+# When player leaves start timer -R
 func _on_detection_area_body_exited(body):
 	if not is_instance_of(body, Player):
 		return
-	await get_tree().create_timer(3).timeout
+	timer.start()
+
+# When timer runs out, turn off the plant -R
+func _on_timer_timeout():
 	$AnimatedSprite2D.play("off")
 	lampActive = false
