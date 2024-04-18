9789d1629f855973f014750575d2c8af6c5fc82a
CookieCherith0969 <cookiecherith@gmail.com>
2024-04-18 16:41:29 +1000

    Light texture upgrade, pulsating idle light

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,13 +6,26 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready
-var flasharea = $FlashArea
+var flasharea = $Rotator/FlashArea
 @onready
-var flashlight = $FlashLight 
+var flashlight = $Rotator/FlashLight 
 var light_on = false : set = set_light_on
+
+@onready
+var glowlight = $GlowLight
+@onready
+var glow_max_radius = glowlight.texture_scale/2
+var glow_timer = 0
+const glow_speed = 1.8
+var glow_min_radius = 0.32
+var period = 2*PI/glow_speed
+
+@onready
+var rotator = $Rotator
 
 var num_lights = 0 : set = set_lights
 var lit = false
+
 
 func _input(event):
 	if event.is_action_pressed("Light"):
@@ -22,7 +35,11 @@ func _input(event):
 			light_on = false
 			
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	glow_timer += delta
+	glow_timer = fmod(glow_timer, period)
+	glowlight.texture_scale = max(cos(glow_timer*glow_speed)*glow_max_radius+glow_max_radius, glow_min_radius)
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var horizontal = Input.get_axis("Left", "Right")
@@ -35,7 +52,7 @@ func _physics_process(_delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
-	rotation += get_angle_to(get_global_mouse_position())
+	rotator.rotation = get_angle_to(get_global_mouse_position())
 	
 	
 
@@ -52,11 +69,9 @@ func _on_light_area_body_exited(body):
 		body.on_unlit()
 
 func on_lit():
-	print_debug("Player lit")
 	num_lights += 1
 	
 func on_unlit():
-	print_debug("Player unlit")
 	num_lights -= 1
 	
 func set_lights(val : int):
