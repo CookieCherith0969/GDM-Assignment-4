12f6408d97f2941d0d99ee94a59612b6bfffb6ec
CookieCherith0969 <cookiecherith@gmail.com>
2024-04-28 13:12:43 +1000

    Door restructure and light occluding

--- Scenes/lockedDoor.gd
+++ Scenes/lockedDoor.gd
@@ -1,15 +1,10 @@
 extends Node2D
 
-@onready
-var barrier = $StaticBody2D
-
 # Signal to listen for player entering the door's area
 func _ready():
-	$Area2D.connect("body_entered", Callable(self, "_on_Area2D_body_entered"))
+	pass
 
-# Callback function when player enters the Area2D
-func _on_Area2D_body_entered(body):
+func _on_area_2d_body_entered(body):
 	if body.has_method("check_has_key") and body.check_has_key():
 		body.set_key(false)
 		queue_free()
-
