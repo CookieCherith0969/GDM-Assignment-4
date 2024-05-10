66c7f0fe8bc18ff2eafde7d47a08b3661413390d
ronincampbell <ronincampbell3@gmail.com>
2024-05-10 16:52:51 +1000

    Level Select Fix

--- Scenes/Levels/LevelSelect.gd
+++ Scenes/Levels/LevelSelect.gd
@@ -8,22 +8,24 @@ func _on_back_pressed():
 
 func _on_lvl_0_pressed():
 	SoundManager.start_ambient()
-	get_tree().change_scene_to_file("res://Scenes/Levels/Tutorial.tscn")
+	LevelManager.load_level("Tutorial")
 
 
 func _on_lvl_1_pressed():
 	SoundManager.start_ambient()
-	get_tree().change_scene_to_file("res://Scenes/Levels/BatteryLevel.tscn")
+	LevelManager.load_level("BatteryLevel")
 
 
 func _on_lvl_2_pressed():
 	SoundManager.start_ambient()
-	get_tree().change_scene_to_file("res://Scenes/Levels/LampLevel.tscn")
+	LevelManager.load_level("LampLevel")
 
 
 func _on_lvl_3_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/DavidLevel.tscn")
+	SoundManager.start_ambient()
+	LevelManager.load_level("DavidLevel")
 
 
 func _on_lvl_4_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/KynanLevel.tscn")
+	SoundManager.start_ambient()
+	LevelManager.load_level("KynanLevel")
