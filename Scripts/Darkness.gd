917712eb5b83b6b7a6867155b17199734ddce3ec
CookieCherith0969 <cookiecherith@gmail.com>
2024-05-11 01:06:06 +1000

    Darkness can be disabled for testing

--- Scripts/Darkness.gd
+++ Scripts/Darkness.gd
@@ -1,9 +1,12 @@
 extends CanvasModulate
 
+@export
+var disable = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	show()
+	if !disable:
+		show()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
