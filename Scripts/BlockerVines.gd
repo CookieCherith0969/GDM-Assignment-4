7e0663f9d762c49415e95810b3e66056f5f14242
CookieCherith0969 <cookiecherith@gmail.com>
2024-05-11 01:53:54 +1000

    Vine blocker, big battery level addon

---
+++ Scripts/BlockerVines.gd
@@ -0,0 +1,29 @@
+@tool
+extends StaticBody2D
+
+@export
+var size : Vector2i = Vector2i(16,16) : set = set_size
+
+@onready
+var shape = $CollisionShape2D
+@onready
+var texture = $TextureRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	shape.shape = RectangleShape2D.new()
+	size = size
+
+func set_size(val : Vector2i):
+	size = val
+	if size.x < 16:
+		size.x = 16
+	if size.y < 16:
+		size.y = 16
+	
+	if not is_instance_valid(shape):
+		return
+	
+	shape.shape.size = size
+	shape.position = Vector2(size)/2
+	texture.size = size
