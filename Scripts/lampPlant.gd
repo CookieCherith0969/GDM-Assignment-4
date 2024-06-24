934de0c9b00624ac6884060856c2213e62d420af
CookieCherith0969 <cookiecherith@gmail.com>
2024-06-24 13:07:20 +1000

    Lamp plants now gradually turn off their lights

--- Scripts/lampPlant.gd
+++ Scripts/lampPlant.gd
@@ -3,6 +3,9 @@ extends Node2D
 var lampActive = false : set = set_active
 @onready
 var timer = $Timer
+var light_time = 3
+var light_timer = 0
+var held = false
 @onready
 var sprite = $AnimatedSprite2D
 @onready
@@ -12,14 +15,26 @@ var glowlight = $GlowLight
 @onready var plantlight = $PlantLight
 @onready
 var active_sound = $ActiveSound
+const timer_frames = 3
 
-func _ready():
-	sprite.play("off") # Start with lamp off -R
-
-func _process(_delta):
-	if lampActive:
+func _process(delta):
+	if light_timer > 0:
 		if not active_sound.playing:
 			active_sound.play()
+		if held:
+			return
+		light_timer -= delta
+		
+		if light_timer <= 0:
+			sprite.play("off")
+			lampActive = false
+		else:
+			var progress = (light_time-light_timer)/light_time
+			var animation_num = int(progress*timer_frames)
+			var frame = sprite.frame
+			var frame_progress = sprite.frame_progress
+			sprite.animation = "on"+str(animation_num)
+			sprite.set_frame_and_progress(frame, frame_progress)
 	else:
 		if active_sound.playing:
 			active_sound.stop()
@@ -28,16 +43,19 @@ func _process(_delta):
 func _on_detection_area_body_entered(body):
 	if not is_instance_of(body, Player):
 		return
-	sprite.play("on")
+	held = true
+	light_timer = light_time
+	sprite.play("on0")
 	lampActive = true
 	plantlight.play()
-	timer.stop()
+#	timer.stop()
 
 # When player leaves start timer -R
 func _on_detection_area_body_exited(body):
 	if not is_instance_of(body, Player):
 		return
-	timer.start()
+	held = false
+	#timer.start()
 
 # When timer runs out, turn off the plant -R
 func _on_timer_timeout():
